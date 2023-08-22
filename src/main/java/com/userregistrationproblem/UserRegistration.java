c6bf278c0544976fa79b7493264f57a8ec3d1003
drisyaathul <drisyak809@gmail.com>
2023-08-23 00:29:06 +0800

    Added: User need to enter a valid FirstName

--- src/main/java/com/userregistrationproblem/UserRegistration.java
+++ src/main/java/com/userregistrationproblem/UserRegistration.java
@@ -1,4 +1,10 @@
 package com.userregistrationproblem;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public boolean validFirstName(String firstName) {
+
+        return Pattern.matches("^[A-Z][a-z]{2,}",firstName);
+    }
 }
--- src/test/java/com/userregistrationproblem/UserRegistrationTest.java
+++ src/test/java/com/userregistrationproblem/UserRegistrationTest.java
@@ -1,4 +1,14 @@
 package com.userregistrationproblem;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class UserRegistrationTest {
+    @Test
+    void givenFirstNameReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualFirstName = userRegistration.validFirstName("Drisya");
+        Assertions.assertTrue(actualFirstName);
+        System.out.println("Valid First Name");
+    }
 }
